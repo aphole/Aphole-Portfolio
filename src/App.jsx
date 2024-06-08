d29ca91f7b1cb9f95d6aa2e5b57145dfa050c352
aphole <apholejeansoriano2001@gmail.com>
2024-06-09 00:21:31 +0800

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Projects from './sections/Projects/Projects'
+import Hero from './sections/Hero/Hero'
+import Footer from './sections/Footer/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+     <Hero />
+     <Projects />
+     <Footer />
+    </>
+  )
+}
+
+export default App
